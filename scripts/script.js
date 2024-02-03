69c1b0dbce67e711160d75d07e74bd029faabde9
Vinh911 <nguyenvinh1997@yahoo.de>
2024-02-03 19:32:08 +0700

    Refactor HTML structure and add button styling

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,24 @@ var buttonWidth = noButton.offsetWidth;
 var buttonHeight = noButton.offsetHeight;
 
 function moveNoButton() {
+  noButton.style.position = 'absolute';
+
   var x = Math.floor(Math.random() * (window.innerWidth - buttonWidth));
   var y = Math.floor(Math.random() * (window.innerHeight - buttonHeight)); 
 
   // Update the button's position
   noButton.style.left = x + 'px';
   noButton.style.top = y + 'px';
+
+  setTimeout(function() {
+    noButton.style.position = 'static';
+  }, 1000);
 }
 
 function displayAlert() {
-  alert('See you on 26.02 at 19:00!'); // change this to your needs
+  alert('See you on 26.02 at 19:00!'); // Pass this to your needs
 }
 
 noButton.addEventListener('mouseover', moveNoButton);
-noButton.addEventListener('click', moveNoButton); // in case someone opens it on mobile
-
+noButton.addEventListener('click', moveNoButton); // In case someone opens it on mobile
 yesButton.addEventListener('click', displayAlert);
